3d966af9b0721c68ab1291594767b312a27ebace
Argir Argirov <dj.giro@gmail.com>
2018-05-03 14:11:37 -0400

    api call works hardcoded

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const axios = require('axios');
+
+const currencyApi = (date, baseCurrency, conversionCurrency) => {
+  axios.get(`https://exchangeratesapi.io/api/${date}?base=${baseCurrency}&symbols=${conversionCurrency}`)
+  .then((res) => {
+    console.log(res.data.rates[conversionCurrency]);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
+
+currencyApi('2017-01-12', 'USD', 'CAD')
