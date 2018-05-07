dde927760f41d41dd22e7ec41e63792e8ee1e8fb
Argir Argirov <dj.giro@gmail.com>
2018-05-07 14:33:18 -0400

    throw error for negative amounts

--- server.js
+++ server.js
@@ -6,23 +6,28 @@ const convert = (date, baseCurrency, conversionCurrency, amount) => {
     const rate = res.data.rates[conversionCurrency];
     const date = res.data.date;
     const base_currency = res.data.base_currency;
-    return {
-      "date": date,
-      "base_currency": baseCurrency,
-      "base_amount": amount,
-      "conversion_currency": conversionCurrency,
-      "conversion_amount":  Number((rate*amount).toFixed(2))
+    if (amount <= 0) {
+      throw new Error('amount not greater than 0')
+    }
+    else {
+      return {
+        "date": date,
+        "base_currency": baseCurrency,
+        "base_amount": amount,
+        "conversion_currency": conversionCurrency,
+        "conversion_amount":  Number((rate*amount).toFixed(2))
+      }
     }
   })
   .catch((error) => {
-    console.log('api error', error);
+    console.log('API error', error);
   })
 }
 
 convert('2017-06-4', 'USD', 'CAD', 100) //sample execution
 .then((res) => {
   const jsonRes = JSON.stringify(res) //stringify js object so it's a JSON string
-  console.log('output', jsonRes); //output return for diplay
+  console.log('Output:', jsonRes); //output return for diplay
   return jsonRes
 })
 
