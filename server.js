16d9432e6a3f33561d67145caf9f35a983c34a1f
Argir Argirov <dj.giro@gmail.com>
2018-05-07 13:45:25 -0400

    combine 2 functions into one

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const axios = require('axios');
 
-const currencyApi = (date, baseCurrency, conversionCurrency) => {
+const convert = (date, baseCurrency, conversionCurrency, amount) => {
   return axios.get(`https://exchangeratesapi.io/api/${date}?base=${baseCurrency}&symbols=${conversionCurrency}`)
   .then((res) => {
-    return res.data.rates[conversionCurrency];
-  })
-  .catch((error) => {
-    console.log('api error', error);
-  })
-}
-const convert = (date, baseCurrency, conversionCurrency, amount) => {
-  return currencyApi(date, baseCurrency, conversionCurrency)
-  .then((rate) => { //recieve the rate from the api and construct a json response
+    const rate = res.data.rates[conversionCurrency];
     return {
       "date": date,
       "base_currency": baseCurrency,
@@ -20,9 +12,12 @@ const convert = (date, baseCurrency, conversionCurrency, amount) => {
       "conversion_amount":  Number((rate*amount).toFixed(2))
     }
   })
+  .catch((error) => {
+    console.log('api error', error);
+  })
 }
 
-convert('2017-01-12', 'USD', 'CAD', 100)
+convert('2017-01-12', 'USD', 'CAD', 100) //run with hard coded data
 .then((res) => {
   const jsonRes = JSON.stringify(res) //stringify js object so it's a JSON string
   console.log('output', jsonRes); //output return for diplay
