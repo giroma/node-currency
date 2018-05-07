2c9e13692514be4f44ab4ec5ab4658c9e3f3d9a2
Argir Argirov <dj.giro@gmail.com>
2018-05-07 13:56:09 -0400

    add more expect statements for each mock data output

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,11 +4,11 @@ const nock = require('nock');
 const {convert} = require('../server');//import server.js function
 const mockData = require('./mockData');
 
-describe('Unit test case 1', () => {
+describe('Unit test cases', () => {
   beforeEach(() => {
     nock('https://exchangeratesapi.io') //nock creates a mock api return
       .get(`/api/${mockData.one.date}?base=${mockData.one.base_currency}&symbols=${mockData.one.conversion_currency}`)
-      .reply(200, {"base":mockData.one.base_currency,"date":mockData.one.date,"rates":{"CAD":0.9785}})
+      .reply(200, {"base":mockData.one.base_currency,"date":mockData.one.date,"rates":{"CAD":0.9785}}) //reply with a hardcoded api return in the format of the real api
     nock('https://exchangeratesapi.io')
       .get(`/api/${mockData.two.date}?base=${mockData.two.base_currency}&symbols=${mockData.two.conversion_currency}`)
       .reply(200, {"base":mockData.two.base_currency,"date":mockData.two.date,"rates":{"SEK":13.4819}})
@@ -25,6 +25,11 @@ describe('Unit test case 1', () => {
             mockData.one.conversion_currency,
             mockData.one.base_amount )
       .then((res) => {
+        expect(typeof res).toBe('object')
+        expect(res.date).toBe(mockData.one.date);
+        expect(res.base_currency).toBe(mockData.one.base_currency);
+        expect(res.conversion_currency).toBe(mockData.one.conversion_currency);
+        expect(res.base_amount).toBe(mockData.one.base_amount);
         expect(res.conversion_amount).toBe(mockData.one.conversion_amount);
         done()
       })
@@ -33,8 +38,13 @@ describe('Unit test case 1', () => {
     convert(mockData.two.date,
             mockData.two.base_currency,
             mockData.two.conversion_currency,
-            mockData.two.base_amount )
+            mockData.two.base_amount)
       .then((res) => {
+        expect(typeof res).toBe('object')
+        expect(res.date).toBe(mockData.two.date);
+        expect(res.base_currency).toBe(mockData.two.base_currency);
+        expect(res.conversion_currency).toBe(mockData.two.conversion_currency);
+        expect(res.base_amount).toBe(mockData.two.base_amount);
         expect(res.conversion_amount).toBe(mockData.two.conversion_amount);
         done()
       })
@@ -45,6 +55,11 @@ describe('Unit test case 1', () => {
             mockData.three.conversion_currency,
             mockData.three.base_amount )
       .then((res) => {
+        expect(typeof res).toBe('object')
+        expect(res.date).toBe(mockData.three.date);
+        expect(res.base_currency).toBe(mockData.three.base_currency);
+        expect(res.conversion_currency).toBe(mockData.three.conversion_currency);
+        expect(res.base_amount).toBe(mockData.three.base_amount);
         expect(res.conversion_amount).toBe(mockData.three.conversion_amount);
         done()
       })
@@ -55,6 +70,11 @@ describe('Unit test case 1', () => {
             mockData.four.conversion_currency,
             mockData.four.base_amount )
       .then((res) => {
+        expect(typeof res).toBe('object')
+        expect(res.date).toBe(mockData.four.date);
+        expect(res.base_currency).toBe(mockData.four.base_currency);
+        expect(res.conversion_currency).toBe(mockData.four.conversion_currency);
+        expect(res.base_amount).toBe(mockData.four.base_amount);
         expect(res.conversion_amount).toBe(mockData.four.conversion_amount);
         done()
       })
